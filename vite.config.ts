d2b4f4b36105184f561a61db3f627647382f1515
craig kaiser <craig@kaiserengineering.io>
2022-09-21 20:59:02 -0400

    use Tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import postcss from './postcss.config.cjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  base: "/poker-player/",
+  plugins: [svelte()],
+  css:{
+    postcss
+  }
 })
